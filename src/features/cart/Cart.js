c85c670404b7f358da4fd4e7c9ad371fb6d4b166
shwetank00001 <mshwetank@gmail.com>
2023-11-01 15:53:54 +0530

    fix

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -79,7 +79,7 @@ export default function Cart() {
                                                       </a>
                                                   </h3>
                                                   <p className="ml-4">
-                                                      $
+                                                      ₹
                                                       {
                                                           item.product
                                                               .discountPrice
@@ -165,7 +165,7 @@ export default function Cart() {
                   <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                       <div className="flex justify-between my-2 text-base font-medium text-gray-900">
                           <p>Subtotal</p>
-                          <p>$ {totalAmount}</p>
+                          <p>₹ {totalAmount}</p>
                       </div>
                       <div className="flex justify-between my-2 text-base font-medium text-gray-900">
                           <p>Total Items in Cart</p>
@@ -190,7 +190,7 @@ export default function Cart() {
                                       type="button"
                                       className="font-medium text-indigo-600 hover:text-indigo-500"
                                   >
-                                     Continue Shopping
+                                      Continue Shopping
                                       <span aria-hidden="true"> &rarr;</span>
                                   </button>
                               </Link>
