a8985c8e4758cf45bfbc2aff3065d42339e37f8e
shwetank00001 <mshwetank@gmail.com>
2023-11-05 11:25:00 +0530

    FE changes

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
         elements,
         confirmParams: {
             // Make sure to change this to your payment completion page
-            return_url: `https://shwetank-ecommerce.vercel.app/order-success/${currentOrder.id}`,
+            return_url: `https://shwetank-ecommerce.netlify.app/order-success/${currentOrder.id}`,
         },
     });
 
