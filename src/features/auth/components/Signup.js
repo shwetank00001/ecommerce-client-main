7e2ef194fbd7c2d47f9bfb54e3be2a354677849f
shwetank00001 <mshwetank@gmail.com>
2023-11-07 18:06:43 +0530

    done

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -63,8 +63,10 @@ export default function Signup() {
                       value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
                       message: 'email not valid',
                     },
+                    
                   })}
                   type="email"
+                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 {errors.email && (
